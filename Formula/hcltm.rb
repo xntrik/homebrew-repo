d0e55f8b533c527a3736a1057a195ffff5aab835
xntrik <xntrik@gmail.com>
2021-10-25 07:12:46 +0800

    test branch

--- Formula/hcltm.rb
+++ Formula/hcltm.rb
@@ -4,7 +4,6 @@ class Hcltm < Formula
   url "https://github.com/xntrik/hcltm/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0d9d27ebb8329937e71ebaed4af73c2c2d9a01caf085a6c49a41c71728f31581"
   license "MIT"
-
   head "https://github.com/xntrik/hcltm.git", branch: "main"
 
   depends_on "go" => :build
