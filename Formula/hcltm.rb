05e3643e594cac92053a477114282baf41fd165b
xntrik <xntrik@gmail.com>
2022-02-12 18:55:03 +0800

    bumping to 0.1.1 - try 2

--- Formula/hcltm.rb
+++ Formula/hcltm.rb
@@ -8,6 +8,13 @@ class Hcltm < Formula
 
   depends_on "go" => :build
 
+  bottle do
+    root_url "https://github.com/xntrik/homebrew-repo/releases/download/hcltm-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "e3f206ffe03a8cadb9385a4939e6d318b12eedeff77fc26716bd73593559508d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cae7670c4fc80067272fd855b8d2542487a02b57950ac6b65bfcb1d7dd5eb3cd"
+  end
+
   def install
     system "go", "build", "-o", "bin/hcltm", "./cmd/hcltm"
     bin.install "bin/hcltm"
