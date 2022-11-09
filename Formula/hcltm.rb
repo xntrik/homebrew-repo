7b9649f18c1e7c5ac6d27ef9464691cf40eee636
xntrik <xntrik@gmail.com>
2022-11-09 20:37:44 +0800

    hcltm 0.1.4
    updating to hcltm v0.1.4
    
    Closes #6.
    
    Signed-off-by: Christian Frichot <678260+xntrik@users.noreply.github.com>

--- Formula/hcltm.rb
+++ Formula/hcltm.rb
@@ -1,8 +1,8 @@
 class Hcltm < Formula
   desc "Documenting your Threat Models with HCL"
   homepage "https://github.com/xntrik/hcltm"
-  url "https://github.com/xntrik/hcltm/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "2f2eb0ccc453fdca992abae2c1428fa39d45febb3df5c4ae6526e6d82842d276"
+  url "https://github.com/xntrik/hcltm/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "d3127665f4b2acd3210ebe1eb019f79a29143e150f0c05f5d72cc8d2385538af"
   license "MIT"
   head "https://github.com/xntrik/hcltm.git", branch: "main"
 
