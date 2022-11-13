0b6845ba69aa1603bea140db18305c0c7386995a
Christian Frichot <678260+xntrik@users.noreply.github.com>
2022-11-13 11:42:41 +0000

    hcltm: update 0.1.5 bottle.

--- Formula/hcltm.rb
+++ Formula/hcltm.rb
@@ -7,9 +7,9 @@ class Hcltm < Formula
   head "https://github.com/xntrik/hcltm.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/xntrik/homebrew-repo/releases/download/hcltm-0.1.4"
-    sha256 cellar: :any_skip_relocation, big_sur:      "4a08499bf5fe6c086c00ff557f2adbd53430027c07c43f64ad4a07d906287bc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "54170891b5830083699e892e6c578f937181e0b76232f0f33f435df4ec82c5b6"
+    root_url "https://github.com/xntrik/homebrew-repo/releases/download/hcltm-0.1.5"
+    sha256 cellar: :any_skip_relocation, big_sur:      "7ab41a2f14aae683d0e9dd66e355caf022e14fc8f71a53bc7c390f65c291cc62"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4c33b937957e20092f8eee6b58ccc94b1281bb55ea7b0261fd15cb5abbe595e8"
   end
 
   depends_on "go" => :build
